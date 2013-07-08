143be97cc7a530ced289d92714d58b29e8a7163b
nathy <nr352@hotmail.com>
2013-07-07 20:06:35 -0500

    costo hora consumibles, depreciación de herramientas

--- cosmec/GeneratedFiles/ui_cosmec.h
+++ cosmec/GeneratedFiles/ui_cosmec.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'cosmec.ui'
 **
-** Created: Sat 6. Jul 23:16:48 2013
+** Created: Sun 7. Jul 19:33:44 2013
 **      by: Qt User Interface Compiler version 4.8.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
--- cosmec/GeneratedFiles/ui_formularioserexternos.h
+++ cosmec/GeneratedFiles/ui_formularioserexternos.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'formularioserexternos.ui'
 **
-** Created: Sun 7. Jul 17:38:45 2013
+** Created: Sun 7. Jul 19:33:42 2013
 **      by: Qt User Interface Compiler version 4.8.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
