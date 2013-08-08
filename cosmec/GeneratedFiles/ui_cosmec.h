14e03e0080f9ee611727d1401e812fbf0b79ebf1
Richard Andrade <ra.andrade.alfaro@gmail.com>
2013-08-07 23:01:46 -0500

    bloqueo de valor mensual actividad de trabajo

--- cosmec/GeneratedFiles/ui_cosmec.h
+++ cosmec/GeneratedFiles/ui_cosmec.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'cosmec.ui'
 **
-** Created: Wed 7. Aug 22:42:55 2013
+** Created: Wed 7. Aug 23:00:24 2013
 **      by: Qt User Interface Compiler version 4.8.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
